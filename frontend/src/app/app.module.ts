4f0e03ee321a2b26b037759333e59c8daa6fdc82
Sri Darshana <sridarshana@Sris-MacBook-Air.local>
2020-11-15 19:41:51 +0530

    MDB carousel added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from "@angular/common/http";
-import {DataService} from "./shared/services/data.service"
-import { SyncService } from './shared/services/sync.service';
-import { MarketModule } from './market/market.module';
+import { DataService } from "./shared/services/data.service";
+import { SyncService } from "./shared/services/sync.service";
+import { MarketModule } from "./market/market.module";
 // import { UserIdleModule } from "angular-user-idle";
 import { MDBBootstrapModule } from "angular-bootstrap-md";
-
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -20,9 +20,10 @@ import { MDBBootstrapModule } from "angular-bootstrap-md";
     HttpClientModule,
     // UserIdleModule.forRoot({ idle: 300, timeout: 1, ping: 120 }),
     MarketModule, //since initial route component
-    MDBBootstrapModule.forRoot(),
+    // MDBBootstrapModule.forRoot(),
   ],
   providers: [DataService, SyncService],
+  schemas: [NO_ERRORS_SCHEMA],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
